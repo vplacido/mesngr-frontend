9f7bbc487f29141d63d9c0fa5fa27d5a7ecf737e
vplacido <valentinplacido25@gmail.com>
2020-08-09 22:19:43 -0400

    created the homepage

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+class Home extends react.Component {
+    render() {
+        return (
+            <div>
+                <p>HomePage</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
