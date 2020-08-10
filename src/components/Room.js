a01b0962878e2aed618c57f669e4ece218f097b5
vplacido <valentinplacido25@gmail.com>
2020-08-09 22:23:31 -0400

    created new file to obtain the message data from the api

---
+++ src/components/Room.js
@@ -0,0 +1,31 @@
+import react from 'react'
+
+class Room extends react.Component {
+    constructor() {
+        this.state = {
+            message: {}
+        }
+    }
+
+    getMessageData() {
+
+    }
+
+    getRecieverData() {
+
+    }
+
+    getSenderData() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Room;
