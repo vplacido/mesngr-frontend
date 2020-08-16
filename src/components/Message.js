fccfdbc4f13fdd82308335a80b2fb073544281cf
vplacido <valentinplacido25@gmail.com>
2020-08-15 23:09:08 -0400

    created new file Meassage which will show a single message from the api

---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import react from 'react'
+
+class Message extends react.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Message;
