32e1fbf891adb8c9d05ea1d7aceebb7e6fd0ffec
vplacido <valentinplacido25@gmail.com>
2020-08-15 23:15:06 -0400

    create function that allows for messageContainer to map through all messages and send to Message component

--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -1,10 +1,15 @@
 import react from 'react'
+import Message from './Message'
 
 class MessageContainer extends react.Component {
+    mapMessages(messages) {
+        messages.map(m => <Message message={m} />)
+    }
+
     render() {
         return (
             <div>
-
+                {this.mapMessages()}
             </div>
         )
     }
