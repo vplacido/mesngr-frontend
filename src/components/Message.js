44261cbd431080e15a68ebc97a9718be91c5d73d
vplacido <valentinplacido25@gmail.com>
2020-08-15 23:16:54 -0400

    the message file will now show the specific information that was passed down from the MessageContainer

--- src/components/Message.js
+++ src/components/Message.js
@@ -5,7 +5,9 @@ class Message extends react.Component {
     render() {
         return (
             <div>
-                
+                <p>this.message.message</p>
+                <p>this.message.reciever_id</p>
+                <p>this.message.sender_id</p>
             </div>
         )
     }
