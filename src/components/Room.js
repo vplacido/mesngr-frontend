80f1916d382416eaa66905b12bcbb7d1e440c1b6
vplacido <valentinplacido25@gmail.com>
2020-08-15 23:03:10 -0400

    detailed the deliverable to have a basic version of the app working pulling and posting from the rails api

--- src/components/Room.js
+++ src/components/Room.js
@@ -8,15 +8,19 @@ class Room extends react.Component {
     }
 
     getMessageData() {
-
+        //use a fectch get request to pull the data from the rails api to obatain the messages from the user
     }
 
     getRecieverData() {
-
+        //use another fetch get method to obtain the information of the person recieving the message
     }
 
     getSenderData() {
-        
+        //use the fetch get method to pull the information from the person sending the message
+    }
+
+    sendNewMessage() {
+        //use the fetch post method to create a new message sent from the sender
     }
 
     render() {
