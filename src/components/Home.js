f2a61bdd723b9309061ef7684091d46c49aa9a08
vplacido <valentinplacido25@gmail.com>
2020-08-23 17:23:34 -0400

    created function to test if able to access api from functions in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,23 @@ import Room from './Room'
 class Home extends react.Component {
     constructor() {
         this.state = {
-            sender: {},
-            reciever: {}
+            sender: [],
+            reciever: []
         }
     }
 
     getSender() {
-
+        fetch("https://localhost3000.com/sender")
+        .then(resp => resp.json())
+        .then(json => this.state.sender = json[0])
     }
 
     getReciever() {
-
+        fetch("https://localhost3000.com/reciever")
+        .then(resp => resp.json())
+        .then(json => this.state.reciever = json[0])
     }
-    
+
     render() {
         return (
             <div>
