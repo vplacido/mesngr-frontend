3ad3d9b94e6cf6b28eb7d6837a80711b90c82191
vplacido <valentinplacido25@gmail.com>
2020-09-06 20:35:10 -0400

    added line to Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,12 @@
 import react from 'react'
 
 class Login extends react.Component {
+
+    checkLogin() {
+        fetch("https://localhost300.com/messages")
+        .then(resp => resp.json())
+        .then(json => this.setState({message: this.state.message.push(json[0])}))
+    }
     render() {
         return (
             <div>
