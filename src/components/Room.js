3a2bf6d1e112d9706e2ea40fecfec6aa2cf9e64a
vplacido <valentinplacido25@gmail.com>
2020-09-27 21:59:35 -0400

    added new messages to the home file

--- src/components/Room.js
+++ src/components/Room.js
@@ -4,7 +4,10 @@ import MessageContainer from './MessageContainer'
 class Room extends react.Component {
     constructor() {
         this.state = {
-            message: []
+            message: [],
+            reciever: {},
+            sender: {},
+            newMessage: {}
         }
     }
     //fetch call to all the messages and store them in the state
@@ -17,14 +20,24 @@ class Room extends react.Component {
 
     getRecieverData() {
         //use another fetch get method to obtain the information of the person recieving the message
+        fetch("https://localhost300.com/reciever")
+        .then(resp => resp.json())
+        .then(json => this.setState({reciever: {info: json}}))
     }
 
     getSenderData() {
         //use the fetch get method to pull the information from the person sending the message
+        fetch("https://localhost300.com/sender")
+        .then(resp => resp.json())
+        .then(json => this.setState({sender: {info: json}}))
     }
 
     sendNewMessage() {
+        //create a post method to create a new message
         //use the fetch post method to create a new message sent from the sender
+        // fetch("https://localhost300.com/newMessage")
+        // .then(resp => resp.json())
+        // .then(json => this.setState({sender: {info: json}}))
     }
     //this function will return the entire message of the message container and which will contain an individula message
     render() {
